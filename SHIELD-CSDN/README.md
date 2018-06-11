6dfcdbaf14ae7ff4a656f57eb38916cb3fcd233b
闫杰 <lechengemail@163.com>
2018-06-11 21:36:36 +0800

    shield_csdn

---
+++ SHIELD-CSDN/README.md
@@ -0,0 +1,2 @@
+# SHIELD-CSDN
+chrome plugin
---
+++ SHIELD-CSDN/script.js
@@ -0,0 +1,3 @@
+for (var i = 0; i < document.getElementsByTagName("iframe").length; i++) {
+    document.getElementsByTagName("iframe")[i].style.display = "none";
+}
